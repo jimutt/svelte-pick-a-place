8dae05dc517980dde77c19fdf5fed0fee3383dbb
Jimmy Utterström <jimutt@outlook.com>
2019-08-17 11:13:02 +0200

    Legacy build with IE11 support (#6)
    * Babel config for IE11 support
    
    * npm run build produces multiple outputs where one has IE11 support
    
    * Add information about legacy build in Readme

--- rollup.config.js
+++ rollup.config.js
@@ -4,12 +4,27 @@ import commonjs from 'rollup-plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
+import babel from 'rollup-plugin-babel';
+
+const ie11Build = process.env.PAP_LEGACY_BUILD;
 
 const production = !process.env.ROLLUP_WATCH;
+
 const name = pkg.name
   .replace(/^(@\S+\/)?(svelte-)?(\S+)/, '$3')
   .replace(/^\w/, m => m.toUpperCase())
   .replace(/-\w/g, m => m[1].toUpperCase());
+
+const prodOutput = [];
+if (ie11Build) {
+  prodOutput.push({ file: 'dist/pick-a-place_legacy.min.js', format: 'iife', name });
+} else {
+  prodOutput.push(
+    { file: 'dist/index.min.mjs', format: 'es' },
+    { file: 'dist/index.min.js', format: 'umd', name },
+    { file: 'dist/pick-a-place.min.js', format: 'iife', name }
+  );
+}
 
 export default {
   input: !production ? 'src/main.js' : 'src/components/components.module.js',
@@ -20,17 +35,10 @@ export default {
         name: name,
         file: 'public/bundle.js'
       }
-    : [
-        { file: 'dist/index.min.mjs', format: 'es' },
-        { file: 'dist/index.min.js', format: 'umd', name },
-        { file: 'dist/pick-a-place.min.js', format: 'iife', name }
-      ],
+    : prodOutput,
   plugins: [
     svelte({
-      // enable run-time checks when not in production
       dev: !production,
-      // we'll extract any component CSS out into
-      // a separate file — better for performance
       css: css => {
         production ? css.write('dist/pick-a-place.css') : css.write('public/bundle.css');
       }
@@ -41,6 +49,23 @@ export default {
 
     !production && livereload('public'),
 
+    ie11Build &&
+      babel({
+        extensions: ['.js', '.mjs', '.html', '.svelte'],
+        runtimeHelpers: true,
+        exclude: ['node_modules/@babel/**', 'node_modules/core-js/**'],
+        presets: [
+          [
+            '@babel/preset-env',
+            {
+              targets: '> 1.5%, IE 11, not dead',
+              useBuiltIns: 'usage',
+              corejs: 3
+            }
+          ]
+        ],
+        plugins: []
+      }),
     production && terser()
   ],
   watch: {
